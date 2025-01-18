eac74d9b18a9446ad0d16aefe445e8b6a4ecd2a8
Florian <git@florian-a.dev>
2025-01-18 19:08:03 +0100

    wiuq

---
+++ docker/validator-keys-tool/compilation.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -o errexit  # Exit script on any error
+set -o nounset  # Treat unset variables as errors
+set -o xtrace   # Trace what gets executed (for debugging)
+
+# Clone the repository
+git clone https://github.com/ripple/validator-keys-tool /app
+cd /app
+
+# Create a build directory
+mkdir .build
+cd .build
+
+# Add the Ripple Conan repository
+conan remote add ripple http://18.143.149.228:8081/artifactory/api/conan/conan-non-prod
+
+# Install dependencies
+cmake -DCMAKE_POLICY_DEFAULT_CMP0091=NEW \
+    -DCMAKE_TOOLCHAIN_FILE:FILEPATH=conan_toolchain.cmake \
+    -DCMAKE_BUILD_TYPE=Release \
+    ..
+
+# Build the project
+cmake --build . --parallel 8
